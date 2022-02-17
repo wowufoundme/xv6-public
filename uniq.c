73d93117acf8227ed3720f0ff95192a49cce4160
wowufoundme <shubhkirti.work@gmail.com>
2022-02-17 23:51:49 +0000

    Write functionality for uniq

--- uniq.c
+++ uniq.c
@@ -4,8 +4,8 @@
 
 char buf[512];
 
-void uniq (int fd, char *name) {
-    printf(1, "Entered uniq\n");
+void uniq (int fd, char *name, int cflag) {
+    
     //  Create a 2d array to store the sentences in them
     char arr[20][128];
     int n, x;
@@ -25,7 +25,7 @@ void uniq (int fd, char *name) {
 	    if ( j == 1025 ) j = 0;
         }
     }
-    printf(1, "%c", arr[0][0]);
+    
     // Now we have the lines in 2D array in the format below
     /*
         a b c d e f \n \0 \0 \0
@@ -33,6 +33,39 @@ void uniq (int fd, char *name) {
         a b c d e f \n \0 \0 \0
         d e f \n \0 \0 \0
     */
+
+    int flag = 0, cnt = 1;
+    int k, y;
+
+    for (i = 0; i < 19; i++) {
+    	for (j = 0; j < 128; j++) {
+		if (arr[i][j] == arr[i+1][j]) continue;
+		else {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) cnt++;
+	else {
+		flag = 0;
+		if (cflag == 1) 
+			printf(1, "%d", cnt);
+		cnt = 1;
+		for (k = 0; k < 128; k++) 
+			printf(1, "%c", arr[i][k]);
+	}
+    }
+
+    if (flag == 0) {
+    	if (cflag == 1) printf(1, "%d", cnt);
+    }
+    else {
+    	if (cflag == 1) printf(1, "1");
+	for (y = 0; y < 128; y++) {
+		printf(1, "%c", arr[19][y]);
+	}
+    }
+    exit();
 }
 
 int main (int argc, char *argv[]) {
@@ -45,9 +78,10 @@ int main (int argc, char *argv[]) {
             printf(1, "uniq: cannot open the file %s\n", argv[i]);
             exit();
         }
-	printf(1, "marker 2\n");
-	uniq(fd, argv[i]);
+	
+	uniq(fd, argv[i], 0);
 	exit();
+    
     }
     exit();
 }
